13bc6b5707e7cbc0ccc2afdb42354925439721a5
bearfall <84324334+bearfall@users.noreply.github.com>
2023-10-07 01:31:58 +0800

    1007

--- Assets/AttackEffectGrass.cs
+++ Assets/AttackEffectGrass.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class AttackEffectGrass : MonoBehaviour
 {
     public Transform GrassParent;
+    public Transform VFXGrassParent;
     public GameObject character;
     public List<SpriteRenderer> Grasses;
+    public List<VisualEffect> VFXGrasses;
     // Start is called before the first frame update
     void Start()
     {
         GrassParent = GameObject.Find("草集合").GetComponent<Transform>();
         Grasses = new List<SpriteRenderer>();
         GrassParent.GetComponentsInChildren(Grasses);
+
+        VFXGrassParent = GameObject.Find("VFX草集合").GetComponent<Transform>();
+        VFXGrasses = new List<VisualEffect>();
+        VFXGrassParent.GetComponentsInChildren(VFXGrasses);
     }
 
     // Update is called once per frame
@@ -34,6 +41,22 @@ public class AttackEffectGrass : MonoBehaviour
         }
 
     }
+
+
+
+    public void ShakeVFXGrass()
+    {
+        foreach (var VFXgrass in VFXGrasses)
+        {
+           
+                VFXgrass.SetFloat("GrassShake", 5);
+            
+
+        }
+
+    }
+
+
     public void StopShakeGrass()
     {
         foreach (var grass in Grasses)
